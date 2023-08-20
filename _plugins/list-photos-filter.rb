2d1cb9b7e3536c40b2f4b421a2f4f0acf68ba5aa
Keith Jenkins <kgjenkins@gmail.com>
2023-08-20 01:51:04 -0400

    again

--- _plugins/list-photos-filter.rb
+++ _plugins/list-photos-filter.rb
@@ -2,7 +2,7 @@ module Jekyll
   module ListPhotos
     def list_photos(town, filetype="jpg")
       files = Dir
-        .glob('/' + town + '/*.' + filetype)
+        .glob('/photos/' + town + '/*.' + filetype)
         .select { |e| File.file? e }
         .join(",")
     end
